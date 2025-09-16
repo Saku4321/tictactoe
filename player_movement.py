baf8b1fbc4dcf5b7835bd6059c2c330ed41c0ec6
Jakub <01197854@pw.edu.pl>
2025-09-16 15:21:01 +0200

    2players mod added, also clearing terminal added but it need more some changes

--- player_movement.py
+++ player_movement.py
@@ -1,8 +1,12 @@
 import movement_check
+import os
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 def picking_row(rows):
     print('Please select row where you want to pick line: 1, 2 or 3')
     while True:
         u = input().strip()
+        clear_screen()
         if u in ('1', '2', '3'):
             row = int(u)-1
             if movement_check.check_if_movement_is_possible(rows[row]) is False:
@@ -15,6 +19,7 @@ def picking_place_in_row(rows, row_idx):
     print('Pick place in row where you want to pick line: 1,2,3')
     while True:
         u = input().strip()
+        clear_screen()
         if u in ('1', '2', '3'):
             col = int(u) - 1
             if row[col] == ' ':
@@ -24,3 +29,30 @@ def picking_place_in_row(rows, row_idx):
                 print('This place is taken.Try to choose another place.')
         else:
             print('Pick from 1, 2 or 3')
+def picking_place_in_row_for_second_player(rows, row_idx):
+    row = rows[row_idx]
+    print('Pick place in row where you want to pick line: 1,2,3')
+    while True:
+        u = input().strip()
+        clear_screen()
+        if u in ('1', '2', '3'):
+            col = int(u) - 1
+            if row[col] == ' ':
+                row[col] = 'O'
+                return
+            else:
+                print('This place is taken.Try to choose another place.')
+        else:
+            print('Pick from 1, 2 or 3')
+
+def selecting_game_mode():
+    while True:
+        print('Select game mode: \n1player\n2player')
+        game_mode = input().strip()
+        clear_screen()
+        if game_mode == '1player':
+            return 1
+        elif game_mode == '2player':
+            return 2
+        else:
+            print('Wrong game mode. Please try again.')
--- tictactoe.py
+++ tictactoe.py
@@ -8,14 +8,38 @@ r1 = [' ',' ',' ']
 r2 = [' ',' ',' ']
 r3 = [' ',' ',' ']
 rows = [r1,r2,r3]
-display(rows)
-while True:
-    row_idx=player_movement.picking_row(rows)
-    player_movement.picking_place_in_row(rows,row_idx)
-    display(rows)
-    if(movement_check.wincheck(rows)==True): break
-    ai_movement.ai_move(rows)
-    display(rows)
-    if(movement_check.wincheck(rows)==True): break
+game_mode= player_movement.selecting_game_mode()
+if(game_mode==1):
+    while True:
+        player_movement.clear_screen()
+        display(rows)
+        row_idx=player_movement.picking_row(rows)
+        player_movement.clear_screen()
+        display(rows)
+        player_movement.picking_place_in_row(rows,row_idx)
+        player_movement.clear_screen()
+        display(rows)
+        if(movement_check.wincheck(rows)==True): break
+        ai_movement.ai_move(rows)
+        player_movement.clear_screen()
+        display(rows)
+        if(movement_check.wincheck(rows)==True): break
+if(game_mode==2):
+    while True:
+        player_movement.clear_screen()
+        print('X moves')
+        display(rows)
+        row_idx=player_movement.picking_row(rows)
+        player_movement.clear_screen()
+        display(rows)
+        player_movement.picking_place_in_row(rows,row_idx)
+        player_movement.clear_screen()
+        display(rows)
+        if(movement_check.wincheck(rows)==True): break
+        print('O moves')
+        row_idx=player_movement.picking_row(rows)
+        player_movement.picking_place_in_row_for_second_player(rows,row_idx)
+        display(rows)
+        if(movement_check.wincheck(rows)==True): break
 
 
